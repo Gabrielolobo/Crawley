8b94604f8d8a16745dfc76629f131302935a7fce
Gabriel Lobo <gabrielolobo42@gmail.com>
2023-06-01 15:00:06 -0300

    fix init subclass

--- crawley/crawlers/crawler.py
+++ crawley/crawlers/crawler.py
@@ -1,24 +1,29 @@
 # Abstract Crawler
 from abc import ABC, abstractmethod
+from typing import List
+
 from .result import Result
 
 
 class Crawler(ABC):
+
+    URL = None
+
     def __init__(self):
         self.url = self.URL
 
-    def __init_subclass__(cls, *args, **kwargs):
-        assert cls.URL
+    def __init_subclass__(cls):
+        assert cls.URL is not None, f"URL must be defined in {cls.__name__}"
 
     @abstractmethod
-    def _parse(self, main_page) -> list[Result]:
+    def _parse(self, main_page) -> List[Result]:
         pass
 
     @abstractmethod
     def _get(self):
         pass
 
-    def run(self) -> list[Result]:
+    def run(self) -> List[Result]:
         main_page = self._get()
         results = self._parse(main_page)
         return results
