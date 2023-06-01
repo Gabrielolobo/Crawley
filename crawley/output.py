b624da253377324db43d0027b34f5de9a1608689
Gabriel Lobo <gabrielolobo42@gmail.com>
2023-06-01 17:16:52 -0300

    fix save json

--- crawley/output.py
+++ crawley/output.py
@@ -1,6 +1,5 @@
-# import os
-# import json
-# import csv
+import json
+import csv
 from pprint import pprint
 
 
@@ -17,9 +16,37 @@ def _print(results, *args):
     pprint(results)
 
 
+def _serialize_result(result):
+    serialized_result = {
+        'cpu': result.cpu,
+        'monthly_cost': result.monthly_cost,
+        'memory': result.memory,
+        'storage': result.storage,
+        'bandwidth': result.bandwidth
+    }
+    return serialized_result
+
+
 def _save_json(results, *args):
-    pass
+    if len(args) > 0:
+        filename = args[0]
+    else:
+        filename = 'output.json'
+    serialized_results = [_serialize_result(result) for result in results]
+    with open(filename, 'w') as file:
+        json.dump(serialized_results, file)
 
 
 def _save_csv(results, *args):
-    pass
+    if len(args) > 0:
+        filename = args[0]
+    else:
+        filename = 'output.csv'
+    if isinstance(results, list) and len(results) > 0 and isinstance(results[0], dict):
+        keys = results[0].keys()
+        with open(filename, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=keys)
+            writer.writeheader()
+            writer.writerows(results)
+    else:
+        print("Invalid data format. Results should be a list of dictionaries.")
