948359bf53a7a6f7f6e1e427530a84c2b6f6ad37
Gabriel Lobo <gabrielolobo42@gmail.com>
2023-06-01 14:57:52 -0300

    def clean_element

--- crawley/crawlers/vultr.py
+++ crawley/crawlers/vultr.py
@@ -1,6 +1,9 @@
 # Concrete Vultr Crawler
+
+from typing import List
 import requests
 from bs4 import BeautifulSoup
+# import pdb
 
 from .crawler import Crawler
 from .result import Result
@@ -10,29 +13,46 @@ class VultrCrawler(Crawler):
 
     URL = "https://www.vultr.com/products/bare-metal/#pricing"
 
-    def _parse(self, main_page) -> list[Result]:
-        soup = BeautifulSoup(main_page, 'html.parser')
-        cpu = soup.find_all("h3", "package__title h6 center")
-        ram = soup.find_all("MEMORY")
-        ssd = soup.find_all("STORAGE")
-        band = soup.find_all("BANDWIDTH")
-        price = soup.find_all("PRICE")
+    def _parse(self, main_page) -> List[Result]:
+        if main_page is None:
+            return []
 
-        return [
-            Result(
-                process_unit=cpu.text,
-                memory=ram.text,
-                storage=ssd.text,
-                transfer=band.text,
-                monthly_cost=price.text
-            ),
-        ]
+        soup = BeautifulSoup(main_page, 'html.parser')
+        cards = soup.find_all("a", "package package--boxed package--shadow")
+
+        results = []
+
+        for card in cards:
+            cpus = card.find("h3", "package__title h6 center").text.strip()
+            price = card.find("span", "price__value").text.strip()
+            ram = self.clean_element(card, 'li:-soup-contains("Memory")')
+            ssd = self.clean_element(card, 'li:-soup-contains("SSD")')
+            band = self.clean_element(card, 'li:-soup-contains("Bandwidth")')
+
+            # pdb.set_trace()
+
+            result = Result(
+                cpu=cpus,
+                monthly_cost=price,
+                memory=ram,
+                storage=ssd,
+                bandwidth=band,
+            )
+            results.append(result)
+
+        return results
+
+    def clean_element(self, card, selector):
+        elements = card.select(selector)
+        cleaned_elements = [element.get_text(
+            strip=True) for element in elements]
+        return cleaned_elements
 
     def _get(self):
-        response = requests.get(
-            "https://www.vultr.com/products/bare-metal/#pricing", timeout=0.2)
+        response = requests.get(self.URL, timeout=None)
 
         if response.status_code == 200:
-            return
+            print("Vultr Successful Request")
+            return response.text
         else:
-            print(response.status_code)
+            print("Vultr HTTPS Error", response.status_code)
